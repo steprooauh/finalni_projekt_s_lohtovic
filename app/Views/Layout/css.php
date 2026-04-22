9ba98bec4d8eb1c37283714a02f98b80f115d281
steprooauh <prokop_stepan@oauh.cz>
2026-04-22 09:14:46 +0200

    layout_css_js

--- app/Views/Layout/css.php
+++ app/Views/Layout/css.php
@@ -1,1 +1,3 @@
-<link rel="stylesheet" href="<?= base_url('node_modules/bootstrap/dist/css/bootstrap.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/bootswatch/dist/quartz/bootstrap.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/@fontawesome/fontawesome-free/css/all.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/flag-icons/css/flag-icons.min.css'); ?>">
