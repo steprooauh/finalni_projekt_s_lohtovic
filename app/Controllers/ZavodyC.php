e4176497e975334aaf437872592cc3b6e11b5beb
steprooauh <prokop_stepan@oauh.cz>
2026-05-06 08:33:04 +0200

    zavody_tencontrol

--- app/Controllers/ZavodyC.php
+++ app/Controllers/ZavodyC.php
@@ -5,10 +5,30 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\RaceYear;
+use CodeIgniter\HTTP\RequestInterface;
+use Psr\Log\LoggerInterface;
+use Override;
+
 class ZavodyC extends BaseController
 {
+    protected $raceYear;
+
+    #[Override]
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        parent::initController($request, $response, $logger);
+        $this->raceYear = new RaceYear();
+    }
+
     public function index($num)
     {
-        echo view('zavody');
+        $zavody = $this->raceYear->where('year', $num)->findAll();
+
+        $data = [
+            "zavody" => $zavody
+        ];
+
+        echo view('zavody', $data);
     }
 }
