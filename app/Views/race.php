54231babff71d2039c7efbdcf0d6ebe1a7853be0
steprooauh <prokop_stepan@oauh.cz>
2026-05-20 09:22:15 +0200

    dfsfsafdafdf

--- app/Views/race.php
+++ app/Views/race.php
@@ -15,10 +15,15 @@ echo ($this->section('content'));
 <h1 class="text-center">Závod: <?= $zavod->default_name; ?></h1>
 
 <div class="d-flex justify-content-between align-items-center mb-4">
-    <a class="btn btn-secondary" href="<?= base_url('roky') .'/' . $year ?>">
+    <a class="btn btn-secondary" href="<?= base_url('index.php/roky') .'/' . $year ?>">
         <i class="fa-solid fa-caret-left"></i> Zpět
     </a>
 </div>
 
+<div class="card">
+  <div class="card-header">Rok: <?= $year ?></div>
+  <div class="card-body">Nějaký info o tom</div>
+  <div class="card-footer">Logo</div>
+</div>
 
 <?= $this->endSection(); ?>
